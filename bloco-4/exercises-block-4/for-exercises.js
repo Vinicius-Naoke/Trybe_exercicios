12fabb6d98e9e21924a61be3df1af9870143eb9b
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-04-23 15:06:46 -0300

    exercisios array e for

---
+++ bloco-4/exercises-block-4/for-exercises.js
@@ -0,0 +1,11 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let printNames of names) {
+    console.log(printNames)
+}
